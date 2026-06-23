f9de3bc2a2e7c7b9d0c1b4dd69415aa90ea1887e
s425514-netizen <s425514@mailg.shumei-u.ac.jp>
2026-06-23 13:13:17 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: us-east-1_0kax3USWt'', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 3hm31od1rva8pa4biah1g4n5sr'', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:us-east-1_0kax3USWt'', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:3hm31od1rva8pa4biah1g4n5sr'', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: us-east-1'' // e.g. us-east-2
     },
     api: {
